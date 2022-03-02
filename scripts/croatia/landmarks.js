ba7a51e7a1d07846d994a54c1edf6249ef11aaaa
e-bierschenk <bushanks@gmail.com>
2022-03-02 15:53:14 -0600

    added croatia info to database

--- scripts/croatia/landmarks.js
+++ scripts/croatia/landmarks.js
@@ -1,26 +1,18 @@
-const japanLandmarks = [
+const croatiaLandmarks = [
     {
-        landmarkName: "landmark name here",
-        landmarkYearCreated: "1985",
-        landmarkDescription: "landmark description here",
-        landmarkSiteLink: "link to landmark site here"
-                
+        landmarkName: "Kvarner Riviera",
+        landmarkYearCreated: "10,000,000,000 B.C.",
+        landmarkDescription: "The Kvarner Riviera is located next to Istria (along the Croatian coast) and is centred around the city of Rijeka, the busiest port on the Adriatic. The most famous resort in this area is Opatija, with Lovran, Crikvenica, Kraljevica and Novi Vinodolski also popular. The islands of Krk, Losinj, Cres and Rab are also part of the Kvarner region.",
+        landmarkSiteLink: "https://en.wikipedia.org/wiki/Rijeka" 
     },
     {
-        landmarkName: "landmark name here",
-        landmarkYearCreated: "1985",
-        landmarkDescription: "landmark description here",
-        landmarkSiteLink: "link to landmark site here"
-    },
-    {
-        landmarkName: "landmark name here",
-        landmarkYearCreated: "1985",
-        landmarkDescription: "landmark description here",
-        landmarkSiteLink: "link to landmark site here"
+        landmarkName: "Krka National Park",
+        landmarkYearCreated: "est. 1985",
+        landmarkDescription: "Krka National Park is one of the Croatian national parks, named after the river Krka (ancient Greek: Kyrikos) that it encloses. It is located along the middle-lower course of the Krka River in central Dalmatia, in Šibenik-Knin county, downstream Miljevci area, and just a few kilometers northeast of the city of Šibenik. It was formed to protect the Krka River and is intended primarily for scientific, cultural, educational, recreational, and tourism activities. It is the seventh national park in Croatia and was proclaimed a national park in 1985.",
+        landmarkSiteLink: "https://en.wikipedia.org/wiki/Krka_National_Park"
     }
-
 ]
 
-export const getJapanLandmarks = () => {
-    return japanLandmarks;
+export const getCroatiaLandmarks = () => {
+    return croatiaLandmarks;
 }
