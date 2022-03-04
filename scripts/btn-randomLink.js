530745a9362c9a50535c39390f88957894ac8a11
Nayhee <nathantraczewski@gmail.com>
2022-03-04 12:01:56 -0600

    Added landing page, landing page css, script for button

---
+++ scripts/btn-randomLink.js
@@ -0,0 +1,28 @@
+export const getRandomOnetoFour = () => {
+    const random = Math.floor(Math.random() * 4 + 1)
+    return random
+}
+
+export const getLinkURL = () => {
+    let linkURL = ''
+    const random = getRandomOnetoFour()
+    switch (random) {
+        case 1:
+            linkURL = "./croatia/index.html"
+            break;
+        case 2:
+            linkURL = "./france/index.html"
+            break;
+        case 3:
+            linkURL = "./india/index.html"
+            break;
+        case 4:
+            linkURL = "./japan/index.html"
+            break;
+    }
+    return linkURL
+}
+
+export const injectURL = (element) => {
+    element.href = getLinkURL()
+}
